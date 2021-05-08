8f1549367d988f822754ee0cc44d1b0a59e18d30
lotanh <lotan3600@gmail.com>
2021-05-08 17:48:28 +0300

    Finished up with the style, added a music for the background

---
+++ script.js
@@ -0,0 +1,76 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#Time-left')
+const score = document.querySelector('#score')
+const btn = document.querySelector('#btn')
+const gameCard = document.querySelector('.game-card')
+document.getElementById("myAudio").loop = true;
+
+let result = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
+
+
+
+function randomSquare() {
+
+   squares.forEach(square => {
+       square.classList.remove('mole')
+   })
+
+   let randomSquare = squares[Math.floor(Math.random() * 9)]
+   randomSquare.classList.add('mole')
+   hitPosition = randomSquare.id
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+      if (square.id == hitPosition) {
+        result++
+        score.textContent = result
+        hitPosition = null
+      }
+    })
+})
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 700)
+}
+// moveMole()
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime == 0) {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('GAME OVER! Your final score is ' + result)
+        btn.textContent = "PLAY AGAIN!"
+        btn.style.display = null;
+    }
+}
+
+
+btn.addEventListener('click' , () => {
+    gameCard.style.display = null;
+    btn.style.display = 'none';
+    moveMole()
+    function countDown() {
+        currentTime--
+        timeLeft.textContent = currentTime
+    
+        if (currentTime == 0) {
+            clearInterval(countDownTimerId)
+            clearInterval(timerId)
+            alert('GAME OVER! Your final score is ' + result)
+            gameCard.style.display = 'none';
+            btn.textContent = "PLAY AGAIN!"
+            btn.style.display = null;
+            currentTime = 60
+            result = 0
+        }
+    }
+    let countDownTimerId = setInterval(countDown, 700)
+})
